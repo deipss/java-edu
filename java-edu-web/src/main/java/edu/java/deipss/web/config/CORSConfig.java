88ce27e7e8b9908261ee42e57e88681427401f24
deipss <deipss@qq.com>
2022-06-26 20:44:19 +0800

    迁移部分

---
+++ java-edu-web/src/main/java/edu/java/deipss/web/config/CORSConfig.java
@@ -0,0 +1,24 @@
+package edu.java.deipss.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 跨域的url配置
+ */
+@Configuration(proxyBeanMethods = false)
+public class CORSConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**");
+            }
+
+        };
+    }
+}
---
+++ java-edu-web/src/main/java/edu/java/deipss/web/config/ControllerAdviceConfig.java
@@ -0,0 +1,32 @@
+package edu.java.deipss.web.config;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 用于web入口进入的程序，执行过程中的异常捕获
+ */
+@ControllerAdvice(basePackages = "edu.deipss.web.config.controller")
+public class ControllerAdviceConfig extends ResponseEntityExceptionHandler {
+
+    @ResponseBody
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleControllerException(HttpServletRequest request, Throwable ex) {
+        HttpStatus status = getStatus(request);
+        return new ResponseEntity<>(null);
+    }
+
+    private HttpStatus getStatus(HttpServletRequest request) {
+        Integer code = (Integer) request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        HttpStatus status = HttpStatus.resolve(code);
+        return (status != null) ? status : HttpStatus.INTERNAL_SERVER_ERROR;
+    }
+
+}
