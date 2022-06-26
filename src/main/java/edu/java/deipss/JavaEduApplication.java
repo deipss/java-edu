3673a8438cbc3a24ef1f954d192e99d2b59e9317
deipss <deipss@qq.com>
2022-06-26 20:30:51 +0800

    first commit

---
+++ src/main/java/edu/java/deipss/JavaEduApplication.java
@@ -0,0 +1,13 @@
+package edu.java.deipss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaEduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaEduApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/java/deipss/JavaEduApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.java.deipss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaEduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
