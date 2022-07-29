39bf42f7476642c382e3894c88935b786cbacdd7
deipss <deipss@qq.com>
2022-07-29 23:39:00 +0800

    web

--- java-edu-web/src/main/java/EduApplication.java
+++ java-edu-web/src/main/java/EduApplication.java
@@ -1,7 +1,11 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@ComponentScan("edu.java.deipss")
+
 public class EduApplication {
     public static void main(String[] args) {
         SpringApplication.run(EduApplication.class, args);
