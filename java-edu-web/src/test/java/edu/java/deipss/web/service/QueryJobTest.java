6399537662589f6abfcee1c8d807a278816070a1
deipss <deipss@qq.com>
2022-08-06 22:23:09 +0800

    异步丹东

---
+++ java-edu-web/src/test/java/edu/java/deipss/web/service/QueryJobTest.java
@@ -0,0 +1,28 @@
+package edu.java.deipss.web.service;
+
+import edu.java.deipss.spring.config.ThreadConfig;
+import edu.java.deipss.spring.service.MapperQueryService;
+import edu.java.deipss.sql.config.SchedulingMysqlDataSourceConfig;
+import edu.java.deipss.web.BaseTest;
+import edu.java.deipss.web.controller.MapperController;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootTest(classes = {MapperController.class,
+        SchedulingMysqlDataSourceConfig.class, ThreadConfig.class,
+        MapperQueryService.class
+
+})
+@EnableAsync
+public class QueryJobTest extends BaseTest {
+
+    @Autowired
+    MapperQueryService mapperQueryService;
+
+    @Test
+    public void test(){
+        mapperQueryService.query();
+    }
+}
