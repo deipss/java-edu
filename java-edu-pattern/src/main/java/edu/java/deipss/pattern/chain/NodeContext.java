04ce25839181e0211f874cc4dc81db447a9605ea
deipss <deipss@qq.com>
2022-09-27 23:10:49 +0800

    pattern

---
+++ java-edu-pattern/src/main/java/edu/java/deipss/pattern/chain/NodeContext.java
@@ -0,0 +1,4 @@
+package edu.java.deipss.pattern.chain;
+
+public class NodeContext {
+}
---
+++ java-edu-pattern/src/main/java/edu/java/deipss/pattern/chain/NodeResult.java
@@ -0,0 +1,6 @@
+package edu.java.deipss.pattern.chain;
+
+public class NodeResult {
+
+    private boolean finalSuccess;
+}
