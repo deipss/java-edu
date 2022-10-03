19a7546f74cca901191f2fec26841d9471414039
deipss <deipss@qq.com>
2022-10-03 19:41:30 +0800

    dubbo暂定

--- java-edu-web/src/main/java/EduApplication.java
+++ java-edu-web/src/main/java/EduApplication.java
@@ -1,4 +1,5 @@
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.apache.dubbo.spring.boot.autoconfigure.DubboAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -6,11 +7,11 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DubboAutoConfiguration.class})
 @ComponentScan("edu.java.deipss")
 @EnableAsync(proxyTargetClass = true)
 @EnableScheduling
-@EnableDubbo(scanBasePackages={"edu.java.deipss.spring.dubbo"})
+/// @EnableDubbo(scanBasePackages={"edu.java.deipss.spring.dubbo"})
 public class EduApplication {
     public static void main(String[] args) {
         SpringApplication.run(EduApplication.class, args);
