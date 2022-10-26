f35d568f915152e0e5b71f81cda05777b60b21fe
deipss <deipss@qq.com>
2022-10-26 20:48:49 +0800

    validUtil

--- java-edu-base/src/main/java/edu/java/deipss/base/util/CollectionUtil.java
+++ java-edu-base/src/main/java/edu/java/deipss/base/util/CollectionUtil.java
@@ -6,18 +6,19 @@ import java.util.stream.Collectors;
 
 public class CollectionUtil {
 
-    public  static <T>  List<T> diffKeepRight(Collection<T> a,Collection<T> b){
-        return a.stream().filter(i->!b.contains(a)).collect(Collectors.toList());
+    public  static <T>  List<T> diffKeepRight(Collection<T> left,Collection<T> right){
+        List<T> intersection = intersection(left, right);
+        return right.stream().filter(i->!intersection.contains(i)).collect(Collectors.toList());
     }
 
 
-    public  static <T>  List<T> diffKeepLeft(Collection<T> a,Collection<T> b){
-        return diffKeepRight(b,a);
-    }
+    public  static <T>  List<T> diffKeepLeft(Collection<T> left,Collection<T> right){
+        List<T> intersection = intersection(left, right);
+        return left.stream().filter(i->!intersection.contains(i)).collect(Collectors.toList());    }
 
 
-    public  static <T>  List<T> intersection(Collection<T> a,Collection<T> b){
-        return a.stream().filter(i->b.contains(a)).collect(Collectors.toList());
+    public  static <T>  List<T> intersection(Collection<T> left,Collection<T> right){
+        return left.stream().filter(right::contains).collect(Collectors.toList());
     }
 
 
