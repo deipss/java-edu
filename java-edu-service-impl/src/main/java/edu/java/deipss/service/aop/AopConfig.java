ff0321caf742fbd3cc7e051ad31a24c7076be2d6
deipss <deipss@qq.com>
2023-03-15 21:34:44 +0800

    maven module optimize

--- java-edu-service-impl/src/main/java/edu/java/deipss/service/aop/AopConfig.java
+++ java-edu-service-impl/src/main/java/edu/java/deipss/service/aop/AopConfig.java
@@ -17,13 +17,9 @@ import org.springframework.stereotype.Component;
 public class AopConfig {
     /**
      * 1、execution(): 表达式主体。
-     * <p>
      * 2、第一个*号：表示返回类型，*号表示所有的类型。
-     * <p>
      * 3、包名：表示需要拦截的包名，后面的两个句点表示当前包和当前包的所有子包，spring.aop.component包、子孙包下所有类的方法。
-     * <p>
      * 4、第二个*号：表示类名，*号表示所有的类。
-     * <p>
      * 5、*(..):最后这个星号表示方法名，*号表示所有的方法，后面括弧里面表示方法的参数，两个句点表示任何参数。
      */
     @Pointcut("execution(* edu.java.deipss.service.*.*(..))")
---
+++ java-edu-web/src/test/java/edu/java/deipss/web/db/MybatisGenerator.java
@@ -0,0 +1,65 @@
+package edu.java.deipss.web.db;
+
+import com.baomidou.mybatisplus.generator.FastAutoGenerator;
+import com.baomidou.mybatisplus.generator.config.OutputFile;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+
+@RunWith(SpringRunner.class)
+public class MybatisGenerator {
+
+
+    /**
+     * spring.datasource.item.driver-class-name=com.mysql.jdbc.Driver
+     * spring.datasource.item.url=jdbc:mysql://172.21.192.63:3306/
+     * spring.datasource.item.username=app
+     * spring.datasource.item.password=D+qA1472
+     */
+    @Test
+    public void generateTest() {
+        HashMap<OutputFile, String> objectObjectHashMap = Maps.newHashMap();
+        objectObjectHashMap.put(OutputFile.controller, "");
+        objectObjectHashMap.put(OutputFile.service, "");
+        objectObjectHashMap.put(OutputFile.serviceImpl, "");
+        String ip = "172.21.192.63";
+        String dbName = "frxs_promotion";
+        String uname = "app";
+        String pwd = "D+qA1472";
+
+        ArrayList<String> strings = Lists.newArrayList(
+                "t_activity_preproduct"
+        );
+        List<String> collect = strings.stream().map(i -> i + "").collect(Collectors.toList());
+        FastAutoGenerator.create("jdbc:mysql://" + ip + ":3306/" + dbName, uname, pwd)
+                .globalConfig(builder -> {
+                    builder.author("hxl") // 设置作者
+                            //.enableSwagger() // 开启 swagger 模式
+                            .fileOverride() // 覆盖已生成文件
+                            .outputDir("D:\\workspace\\test\\xsyx-auto-test-payment\\xsyx-auto-test-payment-service-dal\\src\\main\\java\\xsyx\\auto\\test\\payment\\dal\\entity"); // 指定输出目录
+                })
+                .packageConfig(builder -> {
+                    builder.parent("xsyx.auto.test.payment.dal.entity").pathInfo(objectObjectHashMap); // 设置父包名
+                })
+
+                .strategyConfig(builder -> {
+                    builder
+                            .addInclude(collect) // 设置需要生成的表名
+                            .addTablePrefix("t_") // 设置过滤表前缀
+                            .addTableSuffix("_0", "_202201")
+                            .entityBuilder().enableLombok()
+                            .enableTableFieldAnnotation()
+                    ;
+
+                })
+                //.templateEngine(new FreemarkerTemplateEngine()) // 使用Freemarker引擎模板，默认的是Velocity引擎模板
+                .execute();
+    }
+
+}
