db04dd41ec703f77d9074589c08ed6298f8ce12f
deipss <deipss666@gmail.com>
2023-05-11 21:31:55 +0800

    数值去处

--- java-edu-common/src/main/java/edu/java/deipss/common/util/NumberUtil.java
+++ java-edu-common/src/main/java/edu/java/deipss/common/util/NumberUtil.java
@@ -5,9 +5,6 @@ import java.math.RoundingMode;
 
 public class NumberUtil {
 
-    public static long divide(long a, long b) {
-        return a / b;
-    }
 
     public static BigDecimal divideHalfUp(BigDecimal a, BigDecimal b) {
         return a.divide(b, 2, RoundingMode.HALF_UP);
@@ -75,6 +72,19 @@ public class NumberUtil {
 
 
     /**
+     * 提供（相对）精确的除法运算，当发生除不尽的情况时，精确到
+     * 小数点以后10位，以后的数字四舍五入。
+     *
+     * @param v1 被除数
+     * @param v2 除数
+     * @return 两个参数的商
+     */
+    public static long divide(long v1, long v2) {
+        return divide(v1, v2, 2);
+    }
+
+
+    /**
      * 提供（相对）精确的除法运算。当发生除不尽的情况时，由scale参数指
      * 定精度，以后的数字四舍五入。
      *
@@ -91,6 +101,26 @@ public class NumberUtil {
         BigDecimal b2 = BigDecimal.valueOf(v2);
         return b1.divide(b2, scale, RoundingMode.HALF_UP).doubleValue();
     }
+
+    /**
+     * 提供（相对）精确的除法运算。当发生除不尽的情况时，由scale参数指
+     * 定精度，以后的数字四舍五入。
+     *
+     * @param v1    被除数
+     * @param v2    除数
+     * @param scale 表示表示需要精确到小数点以后几位。
+     * @return 两个参数的商
+     */
+    public static long divide(long v1, long v2, int scale) {
+        if (scale < 0) {
+            throw new IllegalArgumentException("The scale must be a positive integer or zero");
+        }
+        BigDecimal b1 = BigDecimal.valueOf(v1);
+        BigDecimal b2 = BigDecimal.valueOf(v2);
+        return b1.divide(b2, scale, RoundingMode.HALF_UP).longValue();
+    }
+
+
 
     /**
      * 提供精确的小数位四舍五入处理。
